ae785fd49772ee16524d832f9934962d9053f448
AdithyakrishnaV <adithyakr007@gmail.com>
2021-09-24 13:29:58 +0530

    malloc C

--- Hackerrank_C/Array_Reversal.c
+++ Hackerrank_C/Array_Reversal.c
@@ -1,5 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, *arr, i;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+    
+    for(i=0; i < num/2; i++){
+        int temp = arr[i];
+        arr[i] = arr[num - 1 -i];
+        arr[num - 1 -i] = temp;
+    }
+    
+    for(i = 0; i < num; i++)
+        printf("%d ", *(arr + i));
+    return 0;
+}
 
 
+                   //malloc 
+                   
 //The “malloc” or “memory allocation” method in C is used to 
 // dynamically allocate a single large block of memory with 
 // the specified size
