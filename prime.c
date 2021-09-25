6dc4e387b6a0a45211e87c49a9330de60a3925e8
AdithyakrishnaV <adithyakr007@gmail.com>
2021-09-25 14:24:37 +0530

    finding prime no or not

---
+++ prime.c
@@ -0,0 +1,17 @@
+int main() 
+{ 
+  int n, i;
+  printf("Enter the number\n");
+  scanf("%d", &n);
+  for(i=2; i<n; i++){
+      if(n%i==0){
+        printf("No");
+        break;
+      } 
+  }
+  if(i==n){
+        printf("Yes");
+      }
+  
+   return 0;
+}
